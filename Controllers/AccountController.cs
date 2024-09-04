808045c190f728c94ef2cbdf34b120535826390f
tue1501 <minhtue22112004@gmail.com>
2024-09-04 13:44:04 +0700

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using PetLand.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PetLand.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string sdt, string matkhau)
+        {
+            PetLandEntities db = new PetLandEntities();
+            // Tìm kiếm khách hàng với số điện thoại và mật khẩu đã nhập
+            var khachHang = db.KhachHangs.FirstOrDefault(kh => kh.sdt == sdt && kh.matkhau == matkhau);
+            if (khachHang != null)
+            {
+                // Nếu tìm thấy khách hàng, lưu thông tin vào Session và chuyển hướng đến trang chính
+                Session["KhachHang"] = khachHang;
+                return RedirectToAction("Index", "Trangchinh");
+            }
+            else
+            {
+                var khachHangTonTai = db.KhachHangs.FirstOrDefault(kh => kh.sdt == sdt);
+                if (khachHangTonTai == null)
+                {
+                    ViewBag.ErrorMessage = "Số điện thoại không chính xác. Vui lòng thử lại.";
+                }
+                else
+                {
+                    ViewBag.ErrorMessage1 = "Mật khẩu không chính xác. Vui lòng thử lại.";
+                }
+                return View();
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(string sdt, string matkhau, string hoten, string gmail,string confirmPassword)
+        {
+            using (PetLandEntities db = new PetLandEntities())
+            {
+                // Kiểm tra xem số điện thoại đã tồn tại chưa
+                var khachhangs = db.KhachHangs.FirstOrDefault(kh => kh.sdt == sdt);
+                if (khachhangs == null)
+                {
+                    if (matkhau != confirmPassword)
+                    {
+                        ViewBag.ErrorMessage3 = "Mật khẩu và xác nhận mật khẩu không khớp.";
+                        return View(); // Trả về view với thông báo lỗi
+                    }
+                    else
+                    {
+                        var newItem = new KhachHang
+                        {
+                            sdt = sdt,
+                            matkhau = matkhau,
+                            hoten = hoten,
+                            gmail = gmail,
+                        };
+                        db.KhachHangs.Add(newItem);
+                        db.SaveChanges();
+                        return RedirectToAction("Login"); // Chuyển hướng sau khi thêm thành công
+                    }
+                }
+                else
+                {
+                    // Nếu số điện thoại đã tồn tại, hiển thị thông báo lỗi
+                    ViewBag.ErrorMessage2 = "Số điện thoại đã tồn tại. Vui lòng thử lại.";
+                    return View(); // Trả lại View cùng với model để giữ nguyên thông tin đã nhập
+                }
+            }
+        }
+
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Remove("KhachHang");
+            FormsAuthentication.SignOut();
+            return View("Login");
+        }
+
+    }
+}
---
+++ Controllers/TrangchinhController.cs
@@ -0,0 +1,120 @@
+﻿using PetLand.Areas.Admin.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace PetLand.Areas.Admin.Controllers
+{
+    public class TrangchinhController : Controller
+    {
+        // GET: Trangchinh
+        public ActionResult Index(string filter)
+        {
+             if (Session["KhachHang"] == null)
+             {
+                    return RedirectToAction("Login", "Account");
+             }
+
+            PetLandEntities db = new PetLandEntities();
+            if (string.IsNullOrEmpty(filter))
+            {
+                List<SanPham> dssanpham = db.SanPhams.ToList();
+                ViewBag.Filter = filter; // Truyền biến filter đến View
+                return View(dssanpham);
+            }
+            List<SanPham> dsSanpham = db.SanPhams.Where(m =>
+            m.tensp.ToLower().Contains(filter) == true ||
+            m.mota.Contains(filter)).ToList();
+            return View(dsSanpham);
+        }
+        public ActionResult Index_loaisp()
+        {
+            PetLandEntities db = new PetLandEntities();
+                List<LoaiSanPham> dssanpham = db.LoaiSanPhams.ToList();
+                return View(dssanpham);
+        }
+        //public ActionResult AddCart(int id)
+        //{
+        //    PetLandEntities db = new PetLandEntities();
+        //    Giohang model1 = db.Giohangs.Find(id);
+        //    return View(model1);
+        //}
+
+        [HttpPost]
+        public ActionResult AddCart(int idSanPham, int idKhachHang, int soluong)
+        {
+            using (PetLandEntities db = new PetLandEntities())
+            {
+                // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
+                var existingItem = db.Giohangs.FirstOrDefault(item => item.idsanpham == idSanPham && item.idkhachhang == idKhachHang);
+
+                if (existingItem != null)
+                {
+                    // Nếu sản phẩm đã có trong giỏ hàng, tăng số lượng lên theo giá trị truyền vào
+                    existingItem.sl += soluong;
+                }
+                else
+                {
+                    // Nếu sản phẩm chưa có trong giỏ hàng, thêm sản phẩm mới với số lượng nhập vào
+                    var newItem = new Giohang
+                    {
+                        idsanpham = idSanPham,
+                        idkhachhang = idKhachHang,
+                        sl = soluong,
+                    };
+                    db.Giohangs.Add(newItem);
+                }
+                // Lưu các thay đổi vào cơ sở dữ liệu
+                db.SaveChanges();
+            }
+            // Chuyển hướng đến trang giỏ hàng
+            return RedirectToAction("detail_sanpham", new { id = idSanPham });
+        }
+
+        public ActionResult detail_sanpham(int id)
+        { 
+            PetLandEntities db = new PetLandEntities();
+            SanPham model1 = db.SanPhams.Find(id);
+            return View(model1);
+        }
+
+        public ActionResult detail_danhgia(int id)
+        {
+            PetLandEntities db = new PetLandEntities();
+            SanPham sanpham = db.SanPhams.Find(id);
+            List<Danhgia> dsdanhgia = db.Danhgias
+                                  .Where(d => d.idSanPham == id)
+                                  .ToList();
+            return View(dsdanhgia);
+        }
+        public ActionResult Order()
+        {
+            PetLandEntities db = new PetLandEntities();
+            List<ChiTietDonHang> dsdonhang = db.ChiTietDonHangs.ToList();
+            return View(dsdonhang);
+        }
+        public ActionResult Order_xacnhan(int id)
+        {
+            PetLandEntities db = new PetLandEntities();
+            List<ChiTietDonHang> dssanpham = db.ChiTietDonHangs
+                                 .Where(d => d.idChiTietDonHang == id)
+                                 .ToList();
+            return View(dssanpham);
+        }
+        public ActionResult cart()
+        {
+            PetLandEntities db = new PetLandEntities();
+            List<Giohang> giohangs = db.Giohangs.ToList();
+            return View(giohangs);
+        }
+        public ActionResult cart_delete(int id)
+        {
+                PetLandEntities db = new PetLandEntities();
+                var updateModel = db.Giohangs.Find(id);
+                db.Giohangs.Remove(updateModel);
+                db.SaveChanges();
+                return RedirectToAction("cart");
+        }
+    }
+}
