2e76ac143388735212f4692c90224bec79b318f4
tue1501 <minhtue22112004@gmail.com>
2024-09-08 01:53:10 +0700

    Add files via upload

---
+++ Models/CartItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetLand.Models
+{
+    public class CartItemViewModel
+    {
+        public int Giohangid { get; set; }
+        public int SanPhamId { get; set; }
+        public string TenSanPham { get; set; }
+        public decimal Gia { get; set; }
+        public int SoLuong { get; set; }
+        public decimal TongGia { get; set; }
+        public int KhachHangid { get; set; }
+    }
+}
---
+++ Models/DiscountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetLand.Models
+{
+    public class DiscountViewModel
+    {
+        public string TenGiamGia { get; set; }
+        public string MoTa { get; set; }
+        public DateTime? NgayBatDau { get; set; }
+        public DateTime? NgayKetThuc { get; set; }
+    }
+}
